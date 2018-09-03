a611068af20b98a3b29248b5f14da353649a4cd6
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-03 23:15:23 +0300

    setting up the port for the server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const express = require('express');
 
+const app = express();
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.PORT || 3000;
 
-console.log(__dirname +  '/../public');
-console.log(publicPath);
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});

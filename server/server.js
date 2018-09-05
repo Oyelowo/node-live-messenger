105059217eb785cabc9f0f703e83d6788257f9c6
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-05 19:56:35 +0300

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Oyelowo',
+        text: 'Hey bro!?',
+        createdAt: new Date().toString()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', (socket) => {
         console.log('User was disconnected');
     });

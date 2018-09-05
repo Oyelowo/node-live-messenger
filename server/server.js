ed0c46b532d9291a454f4e66bc7c8b3d74d4efde
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-05 18:47:08 +0300

    socket.io connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -6,11 +6,22 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+let app = express();
 let server = http.createServer(app);
-const app = express();
+let io = socketIO(server);
 
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('User was disconnected');
+    });
+});
+
+
 
 server.listen(port, () => {
     console.log(`server is up on port ${port}`);

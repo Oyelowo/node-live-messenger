1b62c2625d97b5835a70ad795c0065de8cae8fc3
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-07 14:32:57 +0300

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -13,11 +13,29 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('newMessage', message);
+    // Vanilla Javascript
+    let li = document.createElement('li');
+    li.innerText = `${message.from}: ${message.text}`;
+    document
+        .getElementById('messages')
+        .appendChild(li);
+
+    // jQuery let li = $('<li></li>'); li.text(`${message.from}: ${message.text}`);
+    // $('#messages').append(li)
+
 });
 
-socket.emit('createMessage', {
-    from: 'Sakari',
-    text: 'Mita mene?'
-}, function (data) {
-    console.log('Gotcha! ', data);
-});
+// Vanilla Javascript
+let form = document.getElementById('message-form');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: document .querySelector('[name=message]') .value
+    }, () => {});
+})
+
+// jQuery format jQuery('#message-form').on('submit', function (e) {
+// e.preventDefault(); socket.emit('createMessage', {         from: 'User',
+//    text: jQuery('[name=message]').val()     }, function () {}); });
